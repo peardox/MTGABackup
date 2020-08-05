4eda2a7d887dee5799f94d091eb43bc232aaa63c
Simon Booth <simon@peardox.com>
2020-08-05 02:47:04 +0100

    Buggy treeview

--- mainapp.pas
+++ mainapp.pas
@@ -18,12 +18,17 @@ type
     Label1: TLabel;
     MainMenu1: TMainMenu;
     Memo1: TMemo;
+    Memo2: TMemo;
     MenuItem1: TMenuItem;
     MenuItem2: TMenuItem;
     Panel1: TPanel;
     Panel2: TPanel;
+    Panel3: TPanel;
+    Panel4: TPanel;
+    TreeView1: TTreeView;
     procedure Button1Click(Sender: TObject);
     procedure MenuItem2Click(Sender: TObject);
+    procedure Panel1Resize(Sender: TObject);
   private
 
   public
@@ -33,6 +38,8 @@ type
 var
   Form1: TForm1;
 
+procedure DebugLine(Depth, DebugMax: Integer; txt: String);
+
 implementation
 
 uses
@@ -41,6 +48,12 @@ uses
 {$R *.lfm}
 
 { TForm1 }
+
+procedure DebugLine(Depth, DebugMax: Integer; txt: String);
+begin
+  if Depth = DebugMax then
+    Form1.Memo2.Lines.Add(txt);
+end;
 
 procedure TForm1.Button1Click(Sender: TObject);
 const
@@ -68,10 +81,19 @@ begin
       {$else}
       FileList := RecursiveFileList(TestDir);
       {$endif}
+
       Memo1.Lines.Add(EmptyStr);
       linecount := RecursiveListFiles(FileList, Memo1.Lines);
       Memo1.Lines.Insert(0, IntToStr(linecount) + ' records');
       Memo1.VertScrollBar.Position := 0;
+
+      Memo2.Lines.BeginUpdate;
+      RecursiveListFiles(FileList, TreeView1);
+      Memo2.Lines.EndUpdate;
+      TreeView1.AutoExpand := True;
+
+      Memo2.VertScrollBar.Position := 0;
+
       FileList.Free;
     end
   else
@@ -83,6 +105,11 @@ end;
 procedure TForm1.MenuItem2Click(Sender: TObject);
 begin
   Close;
+end;
+
+procedure TForm1.Panel1Resize(Sender: TObject);
+begin
+  Panel3.Height := Round(Panel1.Height / 2);
 end;
 
 end.
--- recursefiles.pas
+++ recursefiles.pas
@@ -1,46 +1,97 @@
 unit recursefiles;
 
 {$mode objfpc}{$H+}
+{$define showdepth}
 
 interface
 
 uses
-  Classes, SysUtils;
+  Classes, SysUtils, ComCtrls;
 
 function RecursiveFileList(const SearchPath: UnicodeString; FileList: TStringList = nil): TStringList;
 function RecursiveListFiles(FileList: TStringList; Listing: TStrings; Depth: Integer = 0): Integer;
+function RecursiveListFiles(FileList: TStringList; Listing: TTreeView; ParentNode: TTreeNode = nil; Depth: Integer = 0): Integer;
 
 implementation
+uses
+  mainapp;
+
+function RecursiveListFiles(FileList: TStringList; Listing: TTreeView; ParentNode: TTreeNode = nil; Depth: Integer = 0): Integer;
+const
+  DebugMax = 1;
+var
+  idx: Integer;
+  LineCount: Integer;
+  ChildNode: TTreeNode;
+begin
+  if Depth = 0 then
+    Listing.Items.BeginUpdate;
+
+  LineCount := FileList.Count;
+
+  DebugLine(Depth, DebugMax, '============================');
+  DebugLine(Depth, DebugMax, 'Enter :  Depth = ' + IntToStr(Depth) + ', Parent = ' + HexStr(ParentNode));
+
+  for idx := 0 to FileList.Count - 1 do
+    begin
+      DebugLine(Depth, DebugMax, 'FileList : Index = ' + IntToStr(idx) + ', Text = ' + FileList.Strings[idx]);
+      if FileList.Objects[idx] <> nil then
+        begin
+          DebugLine(Depth, DebugMax, 'AddChild');
+          ChildNode := Listing.Items.AddChild(ParentNode, FileList.Strings[idx] + ' (' + IntToStr(Depth) + ') [' + IntToStr(idx) + '] ***');
+          LineCount += RecursiveListFiles(FileList.Objects[idx] as TStringList, Listing, ChildNode, Depth + 1);
+        end
+      else
+        begin
+          DebugLine(Depth, DebugMax, 'Add');
+          Listing.Items.Add(ParentNode, FileList.Strings[idx] + ' (' + IntToStr(Depth) + ') [' + IntToStr(idx) + ']');
+        end;
+
+    end;
+
+  if Depth = 0 then
+    Listing.Items.EndUpdate;
+
+  DebugLine(Depth, DebugMax, 'Exit : Depth = ' + IntToStr(Depth));
+
+  Result := LineCount;
+end;
 
 function RecursiveListFiles(FileList: TStringList; Listing: TStrings; Depth: Integer = 0): Integer;
 var
   idx: Integer;
-  linecount: Integer;
+  LineCount: Integer;
 begin
   if Depth = 0 then
     Listing.BeginUpdate;
 
-  linecount := FileList.Count;
+  LineCount := FileList.Count;
 
   for idx := 0 to FileList.Count - 1 do
     begin
-      if Depth > 0 then
-        if idx > 0 then
-          Listing.Add(StringOfChar(' ', Depth * 2) + FileList.Strings[idx])
-        else
-          Listing.Add(StringOfChar(' ', (Depth - 1) * 2) + '+ ' + FileList.Strings[idx])
-      else
-        Listing.Add(FileList.Strings[idx]);
       if FileList.Objects[idx] <> nil then
         begin
-          linecount += RecursiveListFiles(FileList.Objects[idx] as TStringList, Listing, Depth + 1);
+        {$ifdef showdepth}
+        Listing.Add(StringOfChar(' ', Depth * 2) + '+ ' + FileList.Strings[idx] + ' (' + IntToStr(Depth) + ') ***');
+        {$else}
+        Listing.Add(StringOfChar(' ', Depth * 2) + '+ ' + FileList.Strings[idx]);
+        {$endif}
+        LineCount += RecursiveListFiles(FileList.Objects[idx] as TStringList, Listing, Depth + 1);
+        end
+      else
+        begin
+        {$ifdef showdepth}
+        Listing.Add(StringOfChar(' ', Depth * 2) + '  ' + FileList.Strings[idx] + ' (' + IntToStr(Depth) + ')');
+        {$else}
+        Listing.Add(StringOfChar(' ', Depth * 2) + '  ' + FileList.Strings[idx]);
+        {$endif}
         end;
     end;
 
   if Depth = 0 then
     Listing.EndUpdate;
 
-  Result := linecount;
+  Result := LineCount;
 end;
 
 function RecursiveFileList(const SearchPath: UnicodeString; FileList: TStringList = nil): TStringList;
@@ -69,12 +120,12 @@ begin
             begin
               if ((Name <> '.') and (Name <> '..')) then
                 begin
-                  FileList.AddObject(Name, RecursiveFileList(SearchPath + DirectorySeparator + Name, nil));
+                  FileList.AddObject(Name + ' [Node]', RecursiveFileList(SearchPath + DirectorySeparator + Name, nil));
                 end;
             end
           else
             begin
-              FileList.AddObject(Name, nil);
+              FileList.AddObject(Name + ' [Leaf]', nil);
             end;
         end;
       until FindNext(Info)<>0;
